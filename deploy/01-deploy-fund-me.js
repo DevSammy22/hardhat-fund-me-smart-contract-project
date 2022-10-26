755352035abf94df2eab78bf2ca14abeab0ada38
DevSammy22 <sadedeji1992@yahoo.com>
2022-10-26 03:19:02 +0100

    Implemented Contract Deployment on the Goerli Test Network Programmatically

--- deploy/01-deploy-fund-me.js
+++ deploy/01-deploy-fund-me.js
@@ -1,4 +1,8 @@
 const { network } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+const { verify } = require("../utils/verify");
+//require("dotenv").config();
+
 //In hardhat deploy, we would not be having main function or calling a function
 //When we run hardhat deploy, it would call any function we specify
 // async function deployFunc(hre){
@@ -15,10 +19,36 @@ const { network } = require("hardhat");
 const { networkConfig } = require("../helper-hardhat-config");
 module.exports = async ({ getNamedAccounts, deployments }) => {
     const { deploy, log } = deployments;
-    const { deploye } = await getNamedAccounts();
-    const chainId = network.config.chainId;
+    const { deployer } = await getNamedAccounts();
 
     //if chainId is X, we use address Y
     //if chainId is B, we use address A
-    const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    let ethUsdPriceFeedAddress;
+    const chainId = network.config.chainId;
+    //if (chainId == 31337){
+    if (developmentChains.includes(network.name)) {
+        const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+        ethUsdPriceFeedAddress = ethUsdAggregator.address;
+    } else {
+        ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+    }
+
+    // log("----------------------------------------------------");
+    //log("Deploying FundMe and waiting for confirmations...");
+
+    const fundMe = await deploy("FundMe", {
+        from: deployer,
+        args: [ethUsdPriceFeedAddress], //put price feed address
+        log: true,
+        waitConfirmation: network.config.blockConfirmations || 1,
+    });
+
+    if (
+        !developmentChains.includes(network.name) &&
+        process.env.ETHERSCAN_API_KEY
+    ) {
+        await verify(fundMe.address, [ethUsdPriceFeedAddress]);
+    }
+    log("----------------------------------------");
 };
+module.exports.tags = ["all", "fundme"];
--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ require("solidity-coverage");
 require("hardhat-gas-reporter");
 require("hardhat-deploy");
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || "https";
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
@@ -20,6 +20,7 @@ module.exports = {
             url: GOERLI_RPC_URL,
             accounts: [PRIVATE_KEY],
             chainId: 5,
+            blockConfirmations: 6,
         },
         localhost: {
             url: "http://127.0.0.1:8545/", //LocalHost is different from Hardhart networks
@@ -40,7 +41,7 @@ module.exports = {
         apiKey: ETHERSCAN_API_KEY,
     },
     gasReporter: {
-        enabled: true,
+        enabled: false,
         outputFile: "gas-report.txt",
         noColors: true,
         currency: "USD",
